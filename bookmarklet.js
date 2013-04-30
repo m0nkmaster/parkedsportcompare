c49685f3ad2223d14b88eb995b0c186467b8dfe4
Rob MacDonald <macdor07@MC-S056822.local>
2013-04-30 11:17:28 +0100

    Linted the bookmarklet somewhat.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,55 +1,41 @@
-(function(){
+(function() {
+    "use strict";
 
     // the minimum version of jQuery we want
-    var v = "1.9.1";
+    var v = "1.9.1",
+        done = false;
 
-    // check prior inclusion and version
-    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var script = document.createElement("script");
-        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-        script.onload = script.onreadystatechange = function(){
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                initMyBookmarklet();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    } else {
-        initMyBookmarklet();
-    }
-    
     function initMyBookmarklet() {
         (window.myBookmarklet = function() {
 
             var url = location.href;
             url = url.replace(/callback=[\w-_]*/, 'callback=toString');
-            var domain = domain = url.match(/http:\/\/[\w-.]*\//)[0].replace('http://', '').replace('/', '');
-            var parkedUrl = url.replace('/sport', '/parkedsport');
-            var sportContent;
-            var parkedSportContent;
+            var domain = url.match(/http:\/\/[\w-.]*\//)[0].replace('http://', '').replace('/', ''),
+                parkedUrl = url.replace('/sport', '/parkedsport'),
+                sportContent,
+                parkedSportContent;
 
             var makeContentComparable = function(body) {
 
                 // If we've called a json/jsonp endpoint we may have an object
-                if (typeof(body) == 'object') {
+                if (typeof (body) === 'object') {
                     body = JSON.stringify(body).replace(/\\"/g, '"');
-                    body = body.replace(/},/g, "},\n");
+                    body = body.replace(/\},/g, "},\n");
                 }
 
                 // Acceptable differences
-                body = body.replace(/parkedsport/g, 'sport')
-                body = body.replace(/http:\/\/[\w\.]*.bbc.co.uk/g, '')
+                body = body.replace(/parkedsport/g, 'sport');
+                body = body.replace(/http:\/\/[\w\.]*.bbc.co.uk/g, '');
                 body = body.replace(/sport\/0\//g, 'sport/');
 
                 // Formatting cleanup to aid diff
                 body = body.replace(/>\s*/g, '>').replace(/\s+/g, ' ');
                 
                 // Add new lines after each closing tag
-                body = body.replace(/(<\/[\w-]*>)/g, "$1\n");
+                body = body.replace(/(<\/[\w\-]*>)/g, "$1\n");
 
                 return body;
-            }
+            };
 
             var performDiff = function() {
                 if (parkedSportContent == '') {
@@ -65,11 +51,11 @@
                        visitDiffChecker(parkedSportContent, sportContent);
                     }
                 }
-            }
+            };
 
             var compareHtml = function(a, b) {
                 return a == b;
-            }
+            };
             
             var invalidUrl = function(url) {
                 if (url.search(/http:\/\/[\w\.]+.bbc.co.uk\/sport/) != 0) {
@@ -77,17 +63,17 @@
                 }
                 
                 return false;
-            }
+            };
             
             var visitDiffChecker = function(file1, file2) {
-                jQuery('<form id="visitDiffChecker" method="POST" action="http://www.diffchecker.com/diff" target="_blank">' +
+                $('<form id="visitDiffChecker" method="POST" action="http://www.diffchecker.com/diff" target="_blank">' +
                 '<textarea name="file1">' + file1 + '</textarea>' +
                 '<textarea name="file2">' + file2 + '</textarea>' +
                 '<input type="hidden" value="month" name="storage-options" />' +
                 '</form>').appendTo('body').submit();
                 
                 $('form#visitDiffChecker').remove();
-            }
+            };
             
             if (invalidUrl(url)) {
                 alert("This script only works with *.bbc.co.uk/sport urls.\n\nYou're not running this on a /parkedsport url are you?");
@@ -95,14 +81,14 @@
             }
 
             //Get the body again.
-            var jqxhr = jQuery.ajax({
+            var jqxhr = $.ajax({
               url: url
             });
             
             jqxhr.always(function(data) {
                 sportContent = data;
                 //Get parkedsport body
-                jQuery.ajax(parkedUrl).always(function(data) {
+                $.ajax(parkedUrl).always(function(data) {
 
                   //make html comparable
                   sportContent = makeContentComparable(sportContent);
@@ -113,5 +99,20 @@
             });
             
         })();
+    }
+
+    // check prior inclusion and version
+    if (window.$ === undefined || window.$.fn.jquery < v) {
+        var script = document.createElement("script");
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function() {
+            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+                done = true;
+                initMyBookmarklet();
+            }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+        initMyBookmarklet();
     }
 })();
